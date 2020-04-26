ffd45b2b40899935503320c4e4d4158eb583e7f2
Diego Moreno <iam@diegomoreno.co>
2020-04-26 00:24:51 -0500

    Routes configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { EmployeesComponent } from './pages/employees/employees.component';
+import { NewEmployeesComponent } from './pages/new-employees/new-employees.component';
+import { ViewEmployeeComponent } from './pages/view-employee/view-employee.component';
+import { EditEmployeeComponent } from './pages/edit-employee/edit-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employees', component: EmployeesComponent },
+  { path: 'new-employees', component: NewEmployeesComponent },
+  { path: 'view-employees', component: ViewEmployeeComponent },
+  { path: 'edit-employees', component: EditEmployeeComponent },
+  { path: '',   redirectTo: '/employees', pathMatch: 'full' },
+  { path: '**', component: EmployeesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
