7cb1f7ed4b871afa1755eb79317b39c3c1c35c66
Diego Moreno <iam@diegomoreno.co>
2020-04-26 02:48:11 -0500

    Grid created and sort function configured

--- src/app/pages/employees/employees.component.ts
+++ src/app/pages/employees/employees.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesComponent implements OnInit {
 
+  public elements = [
+    { name: 'Diego', age: 21, username: 'txusblack', hireDate: new Date() },
+    { name: 'Ingrid', age: 22, username: 'ingridgil', hireDate: new Date() },
+    { name: 'Lorena', age: 23, username: 'lorenagil', hireDate: new Date() },
+    { name: 'Miguel', age: 24, username: 'miguelmoreno', hireDate: new Date() },
+    { name: 'José', age: 25, username: 'josemoreno', hireDate: new Date() },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
