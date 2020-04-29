cce3e0b7a1f1ba8e723ab1e9b5d2da865c0a2820
Diego Moreno <iam@diegomoreno.co>
2020-04-29 11:41:38 -0500

    Reactive Forms configured in View Employee Page

--- src/app/pages/view-employee/view-employee.component.ts
+++ src/app/pages/view-employee/view-employee.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Employee } from 'src/app/models/employee';
 import { InteractionsService } from 'src/app/services/interactions/interactions.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-view-employee',
@@ -13,6 +15,7 @@ export class ViewEmployeeComponent implements OnInit {
   public username: string;
   public employee: Employee;
 
+  public jobTitles: Array<string> = [];
   public model: any = {
     onColor: 'primary',
     offColor: 'secondary',
@@ -22,11 +25,32 @@ export class ViewEmployeeComponent implements OnInit {
     size: ''
   };
 
+  public employeeForm: FormGroup;
+
   constructor(
     public route: ActivatedRoute,
     public router: Router,
-    public i: InteractionsService
-  ) { }
+    public i: InteractionsService,
+    public api: ApiService
+  ) {
+    this.employeeForm = this.createFormGroup();
+  }
+
+  createFormGroup() {
+    return new FormGroup({
+      name: new FormControl({ value: '', disabled: true }),
+      birthday: new FormControl({ value: '', disabled: true }),
+      age: new FormControl({ value: '', disabled: true }),
+      country: new FormControl({ value: '', disabled: true }),
+      username: new FormControl({ value: '', disabled: true }),
+      hireDate: new FormControl({ value: '', disabled: true }),
+      status: new FormControl({ value: '', disabled: true }),
+      area1: new FormControl({ value: '', disabled: true }),
+      area2: new FormControl({ value: '', disabled: true }),
+      jobTitle: new FormControl({ value: '', disabled: true }),
+      tipRate: new FormControl({ value: '', disabled: true }),
+    });
+  }
 
   popPage() {
     this.router.navigate(['/']);
@@ -36,6 +60,10 @@ export class ViewEmployeeComponent implements OnInit {
     this.username = this.route.snapshot.queryParamMap.get('username');
     if (this.username) {
       this.employee = this.i.elements.find(el => el.username === this.username);
+      this.employeeForm.patchValue({
+        ...this.employee
+      });
+      this.jobTitles.push(this.employeeForm.value.jobTite);
     }
   }
 
